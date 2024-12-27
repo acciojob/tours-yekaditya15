668e3bad943985ad05282aeab42ea04bbc7ef3e5
yekaditya11 <yekaditya11@gmail.com>
2024-12-27 19:42:34 +0530

    update3

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -3,21 +3,27 @@ import React, { useState } from "react";
 const Tour = ({ tour, onDelete }) => {
   const [showMore, setShowMore] = useState(false);
 
+  const handleSeeMoreClick = () => {
+    setShowMore(!showMore);
+  };
+
   return (
-    <div id={`tour-item-${tour.id}`} className="tour-item">
-      <h3>{tour.name}</h3>
-      <p id={`tour-item-para-${tour.id}`}>
-        {showMore ? tour.info : `${tour.info.slice(0, 200)}...`}
-      </p>
-      <button
-        id={`show-more-btn-${tour.id}`}
-        onClick={() => setShowMore(!showMore)}
-      >
-        {showMore ? "Show Less" : "Show More"}
-      </button>
-      <button id={`delete-btn-${tour.id}`} onClick={() => onDelete(tour.id)}>
-        Delete Tour
-      </button>
+    <div className="tour-item" id={`tour-item-${tour.id}`}>
+      <div id="main">
+        {" "}
+        {/* Ensure #main is wrapping the content */}
+        <h2>{tour.name}</h2>
+        <img src={tour.image} alt={tour.name} />
+        <p id={`tour-item-para-${tour.id}`}>
+          {showMore ? tour.info : `${tour.info.substring(0, 200)}...`}
+        </p>
+        <button id={`see-more-${tour.id}`} onClick={handleSeeMoreClick}>
+          {showMore ? "Show Less" : "See More"}
+        </button>
+        <button id={`delete-btn-${tour.id}`} onClick={() => onDelete(tour.id)}>
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
