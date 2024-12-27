66cfd669a4353522cdb08c00ba8ef1d6b810f17b
yekaditya11 <yekaditya11@gmail.com>
2024-12-27 19:34:45 +0530

    update2

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -3,20 +3,21 @@ import React, { useState } from "react";
 const Tour = ({ tour, onDelete }) => {
   const [showMore, setShowMore] = useState(false);
 
-  const toggleInfo = () => {
-    setShowMore(!showMore);
-  };
-
   return (
-    <div className="tour">
-      <img src={tour.image} alt={tour.name} />
-      <h2>{tour.name}</h2>
-      <p>{showMore ? tour.info : `${tour.info.slice(0, 200)}...`}</p>
-      <button onClick={toggleInfo}>
-        {showMore ? "See less" : "Show more"}
+    <div id={`tour-item-${tour.id}`} className="tour-item">
+      <h3>{tour.name}</h3>
+      <p id={`tour-item-para-${tour.id}`}>
+        {showMore ? tour.info : `${tour.info.slice(0, 200)}...`}
+      </p>
+      <button
+        id={`show-more-btn-${tour.id}`}
+        onClick={() => setShowMore(!showMore)}
+      >
+        {showMore ? "Show Less" : "Show More"}
       </button>
-      <p>Price: ${tour.price}</p>
-      <button onClick={() => onDelete(tour.id)}>Delete Tour</button>
+      <button id={`delete-btn-${tour.id}`} onClick={() => onDelete(tour.id)}>
+        Delete Tour
+      </button>
     </div>
   );
 };
